755b5d1688af1375e6bce90b009d4847fa1a99da
mburrow303 <elio21@comcast.net>
2023-09-08 00:34:16 -0600

    U4_05 Not Completed

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const mensNavbar = document.getElementById('mens');
 const womensNavbar = document.getElementById('womens');
 const displayDiv = document.getElementById('display');
 
-const apiURL = "https://fakestoreapi.com/products/";
+const apiURL = "https://fakestoreapi.com/products";
 
 //* async function
 /*
@@ -18,8 +18,13 @@ async function fakeStore(endpoint) {
 
 //* fakeStore() async function re-written as block body arrow function
 const fakeStore = async(endpoint) => {
-  let result = await fetch(apiURL + `category/` + endpoint);
+  let result = await fetch(apiURL + endpoint);
  let storeData = await result.json();
+ console.log(storeData);
+ displayCards(storeData);
+}
+
+function displayCards(storeData) {
  console.log(storeData);
 }
 
@@ -34,36 +39,35 @@ const fakeStore = async(endpoint) => {
 //* event listeners for each global variable
 cartNavbar.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(apiURL + `/category/cart`);
+  fakeStore(`/category/cart`);
 })
 
 electronicsNavbar.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(apiURL + `/category/electronics?sort=asc`);
+  fakeStore(`/category/electronics?sort=asc`);
 })
 
 jewelryNavbar.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(apiURL + `/category/jewelery?sort=asc`);
+  fakeStore(`/category/jewelery?sort=asc`);
 })
 
 mensNavbar.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(apiURL + `/category/men's clothing?sort=asc`);
+  fakeStore(`/category/men's clothing?sort=asc`);
 })
 
 womensNavbar.addEventListener('click', e => {
   e.preventDefault();
-  fakeStore(apiURL + `/category/women's clothing?sort=asc`);
+  fakeStore(`/category/women's clothing?sort=asc`);
 })
 
 //* onload method
 window.onload = (event) => {
-  fakeStore("cart"); //? empty array
-  fakeStore("electronics?sort=asc");
-  fakeStore("jewelery?sort=asc");
-  fakeStore("men's clothing?sort=asc");
-  fakeStore("women's clothing?sort=asc");
-  fakeStore("products?sort=asc"); //! currently returning an empty array not all products & categories
-  //fakeStore("sort=asc"); //! also returns empty array
+  //fakeStore("/cart"); //? empty array
+  fakeStore("/category/electronics?sort=asc");
+  fakeStore("/category/jewelery?sort=asc");
+  fakeStore("/category/men's clothing?sort=asc");
+  fakeStore("/category/women's clothing?sort=asc");
+  fakeStore("?sort=asc"); //? all products in an array
  }
